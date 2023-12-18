a03e71d3042da806b61c65464b825d7546a2bd90
yoomster <nperenboom@hotmail.com>
2023-12-18 16:00:01 +0100

    Add project files.

---
+++ TextFileUI/Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace TextFileUI
+{
+    class Program
+    {
+        private static IConfiguration _config;
+
+        static void Msin(string[] args)
+        {
+
+
+            Console.ReadLine();
+        }
+        private static void InitializeConfiguration()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+
+            _config = builder.Build();
+
+        }
+
+    }
+}
