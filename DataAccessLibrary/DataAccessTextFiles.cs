ecc9b4de2899eb78ee659ca21fe4d5243df5fc95
yoomster <nperenboom@hotmail.com>
2023-12-19 13:18:03 +0100

    Added Library with folder for models

---
+++ DataAccessLibrary/DataAccessTextFiles.cs
@@ -0,0 +1,22 @@
+﻿using DataAccessLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLibrary
+{
+    public class DataAccessTextFiles
+    {
+        public List<ContactModel> ReadAllRecords(string TextFile)
+        {
+
+        }
+
+        public void WriteAllRecords(List<ContactModel> records, string TextFile)
+        {
+
+        }
+    }
+}
---
+++ DataAccessLibrary/Models/ContactModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLibrary.Models
+{
+    public class ContactModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public List<string> PhoneNumbers { get; set; } = new List<string>();
+        public List<string> EmailAddresses { get; set; } = new List<string>();
+    }
+}
