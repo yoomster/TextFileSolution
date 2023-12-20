092e664b04c11e08827033e8bc955dfc68b63505
yoomster <nperenboom@hotmail.com>
2023-12-20 21:30:11 +0100

    write records method added

--- DataAccessLibrary/DataAccessTextFiles.cs
+++ DataAccessLibrary/DataAccessTextFiles.cs
@@ -14,9 +14,18 @@ namespace DataAccessLibrary
 
         }
 
-        public void WriteAllRecords(List<ContactModel> records, string TextFile)
+        public void WriteAllRecords(List<ContactModel> contacts, string TextFile)
         {
+            List<string> lines = new List<string>();
 
+            foreach(var c in contacts)
+            {
+                lines.Add($"{c.FirstName},{c.LastName},{String.Join(';', c.EmailAddresses)},{String.Join(';', c.EmailAddresses)}");
+            }
+
+            File.WriteAllLines(TextFile, lines);
         }
+
+
     }
 }
